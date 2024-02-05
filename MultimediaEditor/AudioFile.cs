9cd9c99ca1b0895d093ba29e2978bff659332de1
Steal-by <rodnoi.nikolai@mail.ru>
2024-02-05 14:26:37 +0300

    Start project

---
+++ MultimediaEditor/AudioFile.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultimediaEditor
+{
+    [Serializable()]
+    public class AudioFile
+    {
+        private string _url;
+        public string Url
+        {
+            get { return _url; }
+            set { _url = value; }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _totaltime;
+        public int TotalTime
+        {
+            get { return _totaltime; }
+            set { _totaltime = value; }
+        }
+
+        private int _starttime;
+        public int StartTime
+        {
+            get { return _starttime; }
+            set { _starttime = value; }
+        }
+
+        private int _endtime;
+        public int EndTime
+        {
+            get { return _endtime; }
+            set { _endtime = value; }
+        }
+
+        private string _format;
+        public string Format
+        {
+            get { return _format; }
+            set { _format = value; }
+        }
+        private double _left;
+        public double Left
+        {
+            get { return _left; }
+            set { _left = value; }
+        }
+        private double _len;
+        public double Len
+        {
+            get { return _len; }
+            set { _len = value; }
+        }
+    }
+}
---
+++ MultimediaEditor/AudioInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace MultimediaEditor
+{
+    class AudioInfo
+    {
+        public AudioInfo(AudioFile file, string Url)
+        {
+
+
+
+
+            FileInfo info = new FileInfo(Url);
+            file.Name = info.Name;
+            file.Format = info.Extension;
+
+            file.Url = Url;
+
+            double Duration = 0;
+            WMPLib.WindowsMediaPlayer w = new WMPLib.WindowsMediaPlayer();
+            WMPLib.IWMPMedia m = w.newMedia(file.Url);
+            if (m != null)
+            {
+                Duration = m.duration;
+            }
+            w.close();
+            file.TotalTime = Convert.ToInt32(Duration * MainWindow.scaling);
+            file.EndTime = file.TotalTime;
+
+
+        }
+    }
+}
