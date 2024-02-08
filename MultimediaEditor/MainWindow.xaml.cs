58d5aab80e40b2973212085008cff517328946d4
Steal-by <rodnoi.nikolai@mail.ru>
2024-02-08 13:46:58 +0300

    micro

--- MultimediaEditor/MainWindow.xaml.cs
+++ MultimediaEditor/MainWindow.xaml.cs
@@ -11,7 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace MultimediaEditor
 {
@@ -21,9 +22,63 @@ namespace MultimediaEditor
     public partial class MainWindow : Window
     {
         public static double scaling = 3.76;
+        public int flag_mic = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        [Obsolete]
+        private void RecordMicroBut_Click(object sender, RoutedEventArgs e)
+        {
+            Microphone micro = null;
+            string url = "";
+            MenuItem item = (MenuItem)TopPanel.Items[5];
+            if (item.IsChecked == false)
+            {
+                SaveFileDialog textDialog = new SaveFileDialog();
+                textDialog.AddExtension = true;
+                textDialog.InitialDirectory = "c:\\";
+                textDialog.Filter = "AVI files (*.wav)|*.wav";
+                textDialog.FilterIndex = 1;
+                textDialog.DefaultExt = "wav";
+                textDialog.OverwritePrompt = true;
+
+                if (textDialog.ShowDialog() == true)
+                {
+                    try
+                    {
+                        if ((textDialog.FileName) != null)
+                        {
+                            string format = Path.GetExtension(textDialog.FileName);
+                            if (format == ".wav")
+                                url = textDialog.FileName;
+                            else
+                                url = textDialog.FileName + ".wav";
+                            item.IsChecked = true;
+                            flag_mic = 1;
+                            micro = new Microphone(this, url);
+                            micro.StartRecord();
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                    }
+                }
+
+            }
+            else
+            {
+                item.IsChecked = false;
+
+                if (flag_mic != 10)
+                    micro.StopRecord();
+                flag_mic = 0;
+                OpenCvSharp.Cv2.WaitKey(1000);
+
+            }
         }
     }
 }
---
+++ MultimediaEditor/Microphone.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+using System.Windows;
+
+namespace MultimediaEditor
+{
+    class Microphone
+    {
+        MainWindow main;
+        string outputFilename = "";
+        public Microphone(MainWindow wind, string uri)
+        {
+            main = wind;
+            outputFilename = uri;
+        }
+        WaveIn waveIn;
+        WaveFileWriter writer;
+
+        [Obsolete]
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+
+           
+            
+             writer.WriteData(e.Buffer, 0, e.BytesRecorded);
+           
+
+        }
+
+        private void WaveIn_RecordingStopped(object sender, EventArgs e)
+        {
+
+            waveIn.Dispose();
+            waveIn = null;
+            writer.Close();
+            writer = null;
+
+        }
+
+        [Obsolete]
+        public void StartRecord()
+        {
+            try
+            {
+
+                waveIn = new WaveIn();
+
+                waveIn.DeviceNumber = 0;
+
+                waveIn.DataAvailable += WaveIn_DataAvailable;
+
+                waveIn.RecordingStopped += new EventHandler<StoppedEventArgs>(WaveIn_RecordingStopped);
+
+                waveIn.WaveFormat = new WaveFormat(8000, 1);
+
+                writer = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
+
+                waveIn.StartRecording();
+            }
+            catch (Exception)
+            {
+               // main.flag_mic = 10;
+                writer.Dispose();
+                System.IO.File.Delete(outputFilename);
+                MessageBox.Show("Микрофон не подключён");
+
+            }
+        }
+        public void StopRecord()
+        {
+            waveIn.StopRecording();
+        }
+    }
+}
